ad995aa8272d67fdcf65d145c31f117af7b1dc9c
Артём <cushnir.artiom@gmail.com>
2022-04-26 21:39:55 +0300

    Add lesson

--- ObjectsAndMethods/src/Main.java
+++ ObjectsAndMethods/src/Main.java
@@ -4,8 +4,8 @@ public class Main {
         Basket basket = new Basket();
         basket.add("Milk", 40);
         basket.add("bread" , 35 , 2 , 0.7);
-        basket.print("       Содержимое корзины");
-        Arithmetic arithmetic = new Arithmetic(2 , 5);
+        basket.print("       Содержимое корзины :");
+        Arithmetic arithmetic = new Arithmetic(7 , 5);
         arithmetic.launch();
         Printer stringQueue = new Printer();
         stringQueue.Appander("Главное в жизни - никогда не сдаваться. " , "Смысловая нагрузка " , 1);
