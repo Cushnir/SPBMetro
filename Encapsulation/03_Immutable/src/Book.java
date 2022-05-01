3af71f354b03f33ef36e04dbe235b7d460e82317
Артём <cushnir.artiom@gmail.com>
2022-05-01 14:20:41 +0300

    5.7

---
+++ Encapsulation/03_Immutable/src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    private final String name;
+    private final String author;
+    private final int numerOfPages;
+    private final int numerISBN;
+    public Book(String name , String author , int numerOfPages , int numerISBN ){
+        this.name = name;
+        this.author = author;
+        this.numerOfPages = numerOfPages;
+        this.numerISBN = numerISBN;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getAuthor(){
+        return  author;
+    }
+    public  int getNumerOfPages(){
+        return numerOfPages;
+    }
+    public  int getNumerISBN(){
+        return numerISBN;
+    }
+}
