281558204692f70b7624df36663f19454105b1bd
Артём <cushnir.artiom@gmail.com>
2022-05-24 21:37:44 +0300

    как то так)

--- Primitives/IntOrInteger/src/Main.java
+++ Primitives/IntOrInteger/src/Main.java
@@ -3,6 +3,26 @@ public class Main {
         Container container = new Container();
         container.addCount(5672);
         System.out.println(container.getCount());
-
+        System.out.println();
+        System.out.println("Èíòåğâàë Ğóññêîãî àëôàâèòà : ");
+        System.out.println();
+        char firstLetter = 'À';
+        char lastLetter = 'ÿ';
+        char letter¨ = '¨';
+        char letter¸ = '¸';
+        int intLetter_¨ = letter¨;
+        int intLetter_¸ = letter¸;
+        int firstLetterCharacter = firstLetter;
+        int firstLastLetterCharacter = lastLetter;
+        for (int i = firstLetterCharacter; i <= firstLastLetterCharacter; i++) {
+            char c = (char) i;
+            System.out.println(i + " -  " + c);
+            if (c == 'Å') {
+                System.out.println(intLetter_¨ + " -  " + letter¨);
+            }
+            if (c == 'å') {
+                System.out.println(intLetter_¸ + " -  " + letter¸);
+            }
+        }
     }
 }
