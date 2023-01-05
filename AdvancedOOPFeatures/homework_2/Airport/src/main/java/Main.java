28e605958fffe14cbcc339c5004a8d7f25d286f2
Артём <cushnir.artiom@gmail.com>
2023-01-05 22:40:58 +0300

    ap

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,7 +1,15 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,9 +17,21 @@ public class Main {
 
     }
 
-    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+    public static List<Flight> findPlanesLeavingInTheNextTwoHours(@NotNull Airport airport) {
+        return airport.getTerminals()
+                .stream()
+                .map(Terminal::getFlights)
+                .flatMap(List::stream)
+                .filter(flight -> flight.getType() == Flight.Type.DEPARTURE
+                        && (flight.getDate()
+                        .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                        .isAfter(LocalDateTime.now())
+                        && (flight.getDate()
+                        .toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                        .isBefore(LocalDateTime.now().plusHours(2)))
+                .collect(Collectors.toList());
+
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
     }
 
 }
